375a9e563acec7acefb3a90c923889de24b2ae57
1M0reBug <jordan.quagliatini@gmail.com>
2015-07-02 09:03:32 +0200

    Somme test passent

---
+++ FinalTest/Somme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace FinalTest
+{
+    public class Somme
+    {
+        public Boolean PeutCalculer(string p0)
+        {
+            return p0.Contains("+");
+        }
+
+        public int Calculer(int[] values)
+        {
+            return values.Aggregate(0, (current, value) => current + value);
+        }
+
+        public int Calculer(string s)
+        {
+
+            var valuesToMultiply = s.Split('+').Select(
+                s2 =>
+                {
+                    var value = s2.Replace(" ", String.Empty);
+                    return int.Parse(value);
+                }).ToArray();
+            return this.Calculer(valuesToMultiply);
+        }
+    }
+}
