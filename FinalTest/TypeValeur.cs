9076eb62b0b130288a0e15d0fa9fda53d2d5cf35
1M0reBug <jordan.quagliatini@gmail.com>
2015-07-02 08:15:27 +0200

    Test DéfinitUnTypeValeur passe

---
+++ FinalTest/TypeValeur.cs
@@ -0,0 +1,18 @@
+﻿namespace FinalTest
+{
+    public struct TypeValeur
+    {
+
+        private int _valeur;
+        public TypeValeur(int i)
+        {
+            _valeur = i;
+        }
+
+        public int Valeur
+        {
+            get { return _valeur; }
+            set { _valeur = value; }
+        }
+    }
+}
