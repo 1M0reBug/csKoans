4399eb2c25b1ededf17bc3a40815bde7840052db
1M0reBug <jordan.quagliatini@gmail.com>
2015-07-02 09:00:48 +0200

    Multiplication passe

--- FinalTest/Multiplication.cs
+++ FinalTest/Multiplication.cs
@@ -1,6 +1,36 @@
+using System;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Windows.Markup;
+
 namespace FinalTest
 {
     public class Multiplication
     {
+        public Multiplication()
+        {
+        }
+
+        public Boolean PeutCalculer(string p0)
+        {
+            return p0.Contains("*");
+        }
+
+        public int Calculer(int[] values)
+        {
+            return values.Aggregate(1, (current, value) => current * value);
+        }
+
+        public int Calculer(string s)
+        {
+
+            var valuesToMultiply = s.Split('*').Select(
+                s2 =>
+                {
+                    var value = s2.Replace(" ", String.Empty);
+                    return int.Parse(value);
+                }).ToArray();
+            return this.Calculer(valuesToMultiply);
+        }
     }
 }
